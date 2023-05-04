abaa199dcbaa2465e678e44f51e77d2290b6cea9
sarsovUW <132025191+sarsovUW@users.noreply.github.com>
2023-05-04 10:36:14 -0700

    Update capML.py

--- capML.py
+++ capML.py
@@ -20,5 +20,9 @@ def predict():
     # Return the prediction as JSON
     return jsonify(prediction.tolist())
 
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
 if __name__ == '__main__':
     app.run()
