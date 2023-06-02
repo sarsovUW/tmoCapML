8f4a5c171b56ca9b13b8eb1e2863ec0b541ced52
sarsovUW <132025191+sarsovUW@users.noreply.github.com>
2023-06-02 14:38:55 -0700

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def predict():
 
 @app.route('/')
 def home():
-    return render_template('templates/index.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
